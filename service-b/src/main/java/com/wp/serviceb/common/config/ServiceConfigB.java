2fc459c3bf1a64da7ce8abcef655c7196ce011a2
Evgenikaall <59172058+Evgenikaall@users.noreply.github.com>
2024-07-29 00:45:29 +0300

    Added archunit example (#2)
    Adjusted readme file
    Fixed typo in serviceb (from serviseb)

--- service-b/src/main/java/com/wp/serviceb/common/config/ServiceConfigB.java
+++ service-b/src/main/java/com/wp/serviceb/common/config/ServiceConfigB.java
@@ -1,4 +1,4 @@
-package com.wp.serviseb.common.config;
+package com.wp.serviceb.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
